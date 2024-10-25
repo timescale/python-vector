f4a0aa3c91a48dcdf2f8ed6025b3c1f948e382af
Jascha <jascha@timescale.com>
2024-10-25 11:40:59 -0700

    Fix tiktoken request

--- tests/utils.py
+++ tests/utils.py
@@ -7,40 +7,9 @@ vcr_cassette_path = os.path.join(os.path.dirname(__file__), "vcr_cassettes")
 
 
 def remove_set_cookie_header(response: dict[str, Any]):
-    """
-    Removes the Set-Cookie header from a VCR.py response object to improve cassette consistency.
-
-    This function can be used as a before_record callback in your VCR configuration
-    to ensure that Set-Cookie headers are stripped from responses before they are
-    recorded to cassettes.
-
-    Args:
-        response (vcr.request.Response): The VCR.py response object to modify
-
-    Returns:
-        vcr.request.Response: The modified response object with Set-Cookie headers removed
-
-    Example:
-        import vcr
-
-        # Configure VCR with the callback
-        vcr = vcr.VCR(
-            before_record_response=remove_set_cookie_header,
-            match_on=['uri', 'method']
-        )
-
-        with vcr.use_cassette('tests/fixtures/my_cassette.yaml'):
-            # Make your HTTP requests here
-            pass
-    """
-
-    # Get the headers from the response
     headers = response["headers"]
-
-    # Headers to remove (case-insensitive)
     headers_to_remove = ["set-cookie", "Set-Cookie"]
 
-    # Remove Set-Cookie headers if they exist
     for header in headers_to_remove:
         if header in headers:
             del headers[header]
